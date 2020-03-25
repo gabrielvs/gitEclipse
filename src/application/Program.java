3035cf184fb182571bc85f4019bced901026bccd
Gabriel Silva <gabrielvs@outlook.com>
2020-03-25 17:23:45 -0300

    fim do mundo adicionado

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,8 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("bom dia");
 		System.out.println("teste2");
+		
+		System.out.println("fim do mundo ");
 	}
 
 }
